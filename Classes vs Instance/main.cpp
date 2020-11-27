50031d2e664765aa01776978e8d06bcdd6b8db91
Kyle Lovely <kylesphere@gmail.com>
2020-11-27 12:33:55 -0500

    Initial Commit

---
+++ Classes vs Instance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Classes vs Instance
+//
+//  Created by Kyle Lovely on 11/27/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
